358f235f36ca28249920136990318553884977f2
tashfiaislamistila <tashfia.cse@gmail.com>
2022-03-24 00:46:43 -0700

    cart value increase commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import './App.css';
 import AllProducts from './Components/AllProducts/AllProducts';
 import Menubar from './Components/Menubar/Menubar';
 
 
 function App() {
-
+  const [count, setCount] = useState(0);
+  const setCartCount = () => {
+    setCount(count + 1);
+  }
   return (
     <div className="App">
-      <Menubar></Menubar>
-      <AllProducts></AllProducts>
+      <Menubar count={count}></Menubar>
+      <AllProducts setCartCount={setCartCount}></AllProducts>
     </div>
   );
 }
--- src/Components/AllProducts/AllProducts.js
+++ src/Components/AllProducts/AllProducts.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import SingleProduct from '../SingleProduct/SingleProduct';
 import './AllProducts.css'
 
-const AllProducts = () => {
-
+const AllProducts = (props) => {
+    const { setCartCount } = props
     const [products, setProducts] = useState([]);
 
 
@@ -18,7 +18,10 @@ const AllProducts = () => {
             <h1>All Products</h1>
             <div className="row container">
                 {
-                    products.map((pd) => (<SingleProduct key={pd.id} product={pd}></SingleProduct>)
+                    products.map((pd) => (<SingleProduct
+                        setCartCount={setCartCount}
+                        key={pd.id} product={pd}
+                    ></SingleProduct>)
                     )}
 
             </div>
