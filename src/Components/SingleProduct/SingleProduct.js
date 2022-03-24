3ce301ac7fe3a3ca17ffd025252262a460ccbcae
tashfiaislamistila <tashfia.cse@gmail.com>
2022-03-24 00:27:40 -0700

    show data commit

--- src/Components/SingleProduct/SingleProduct.js
+++ src/Components/SingleProduct/SingleProduct.js
@@ -5,9 +5,14 @@ const SingleProduct = (props) => {
     console.log(props.product);
     return (
         <div className='col-md-4'>
-            <div className="card p-2 border">
+            <div className="card p-3 border">
+                <img className='w-50 m-auto' src={props.product.image} alt="" />
                 <h1>{props.product.title.slice(0, 10)}</h1>
-
+                <div className="d-flex justify-content-around">
+                    <button className='btn btn-success'>Add to cart</button>
+                    <button className='btn btn-danger'>Delete</button>
+                    <button className='btn btn-info'>Details</button>
+                </div>
             </div>
 
         </div>
